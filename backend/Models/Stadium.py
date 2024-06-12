8c0128f503cd25bb195cfac9c4a1d65c749eb7ec
SantiSiele <santi.siele@gmail.com>
2024-06-12 20:51:04 -0300

    Fix city name

--- backend/Models/Stadium.py
+++ backend/Models/Stadium.py
@@ -5,6 +5,6 @@ class Stadium(db.Model):
     __tablename__ = 'stadiums'
     id_stadium = db.Column(db.Integer, primary_key=True)
     stadium_name = db.Column(db.String(100), nullable=False)
-    place = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
     photo = db.Column(db.String(1000), nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
