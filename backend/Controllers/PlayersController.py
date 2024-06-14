59002bccd5f390deacbf2b8deee8264c1931e948
SantiSiele <santi.siele@gmail.com>
2024-06-14 18:32:07 -0300

    add functions to test

--- backend/Controllers/PlayersController.py
+++ backend/Controllers/PlayersController.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from Models.Player import Player
+from Models.base import db
 
 player_bp = Blueprint('players', __name__, url_prefix='/players')
 
@@ -23,10 +24,10 @@ def get_all_players():
         print('Error:',error)
         return jsonify({'message: ', error}), 500
     
-@player_bp.route('/<id_player>', methods=['GET'])
-def get_player_by_id(id_player):
+@player_bp.route('/team/<id_country>', methods=['GET'])
+def get_player_by_id(id_country):
     try:
-        player = Player.query.where(player.id_player == id_player).first()
+        player = Player.query.where(player.id_player == id_country).first()
         player_data = {
                 'id_player': player.id_player,
                 'player_name': player.player_name,
@@ -39,3 +40,39 @@ def get_player_by_id(id_player):
     except Exception as error:
         print('Error:',error)
         return jsonify({'message: ', error}), 50
+    
+@player_bp.route('/create_player', methods=["POST"])
+def create_player():
+    try:
+        data = request.json
+        new_player_name = data.get('player_name')
+        new_team = data.get('team')
+        new_photo = data.get('photo')
+        new_country = data.get('country')
+        new_position = data.get('position')
+        
+        new_player = Player(player_name=new_player_name, team=new_team, photo=new_photo, country=new_country, position=new_position)
+        db.session.add(new_player)
+        db.session.commit()
+        return jsonify({'New player': 'Yes'})
+    except Exception as error:
+        print('Error:', error)
+        return jsonify({'message': str(error)}), 500
+
+@player_bp.route('/update_player/<id_player>', methods=["PUT"])
+def update_player(id_player):
+    try:
+        data = request.json
+        player = Player.query.where(player.id_player == id_player).first()
+
+        player.player_name = data.get('player_name', player.player_name)
+        player.team = data.get('team', player.team)
+        player.photo = data.get('photo', player.photo)
+        player.country = data.get('country', player.country)
+        player.position = data.get('position', player.position)
+
+        db.session.commit()
+        return jsonify({'message': 'Player updated successfully'})
+    except Exception as error:
+        print('Error:', error)
+        return jsonify({'message': str(error)}), 500 
