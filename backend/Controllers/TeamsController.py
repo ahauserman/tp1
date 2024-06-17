7c961fffbec891fa7a42a80047aa8111848a1593
SantiSiele <santi.siele@gmail.com>
2024-06-17 19:19:28 -0300

    get players with team

--- backend/Controllers/TeamsController.py
+++ backend/Controllers/TeamsController.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from Models.Team import Country
+from Models.Player import Player
 
 team_bp = Blueprint('teams', __name__, url_prefix='/teams')
 
@@ -9,6 +10,17 @@ def get_all_teams():
         countries = Country.query.all()
         countries_data = []
         for country in countries:
+            players = Player.query.where(Player.country == country.id_country).all()
+            players_data = [
+                {
+                    'id': player.id_player,
+                    'player_name': player.player_name,
+                    'team': player.team,
+                    'photo': player.photo,
+                    'position': player.position
+                } for player in players
+            ]
+            
             country_data = {
                 'id_country': country.id_country,
                 'country_name': country.country_name,
@@ -16,18 +28,23 @@ def get_all_teams():
                 'photo': country.photo,
                 'country_group': country.country_group,
                 'titles': country.titles,
-                'dt': country.dt
-                }
+                'dt': country.dt,
+                'players': players_data if players else []
+            }
             countries_data.append(country_data)
+        
         return jsonify({'Countries': countries_data})
     except Exception as error:
-        print('Error:',error)
-        return jsonify({'message: ', error}), 500
+        print('Error:', error)
+        return jsonify({'message': str(error)}), 500
+
+
     
 @team_bp.route('/<id_country>', methods=['GET'])
 def get_team_by_id(id_country):
     try:
         country = Country.query.where(Country.id_country == id_country).first()
+        players = Player.query.where(Player.country == id_country).all()
         country_data = {
                 'id_country': country.id_country,
                 'country_name': country.country_name,
@@ -35,7 +52,16 @@ def get_team_by_id(id_country):
                 'photo': country.photo,
                 'country_group': country.country_group,
                 'titles': country.titles,
-                'dt': country.dt
+                'dt': country.dt,
+                'players': [
+                {
+                    'id': player.id_player,
+                    'name': player.player_name,
+                    'team': player.team,
+                    'photo': player.photo,
+                    'position': player.position
+                } for player in players
+                ]
         }
         return jsonify({'Country': country_data})
     except Exception as error:
