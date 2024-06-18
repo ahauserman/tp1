76943ca1accd3b035bdcd22fc7e335ecaccf6e55
antonella hauserman <ahauserman@fi.uba.ar>
2024-06-18 13:09:07 -0300

    team.html now renders players!

--- backend/Models/Player.py
+++ backend/Models/Player.py
@@ -3,8 +3,8 @@ from Models.base import db
 
 class Player(db.Model):
     __tablename__ = 'players'
-    id_ = db.Column(db.Integer, primary_key=True)
-    _name = db.Column(db.String(100), nullable=False)
+    id_player = db.Column(db.Integer, primary_key=True)
+    player_name = db.Column(db.String(100), nullable=False)
     team = db.Column(db.String(100), nullable=False)
     photo = db.Column(db.String(1000), nullable=False)
     country = db.Column(db.Integer, nullable=False)
