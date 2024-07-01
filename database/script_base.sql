2f6849c3f65f07fb4b65c56f491ecc66d05a6ad9
SantiSiele <santi.siele@gmail.com>
2024-07-01 14:24:46 -0300

    database with uploaded players

--- database/script_base.sql
+++ database/script_base.sql
@@ -1,3 +1,18 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.12 (Ubuntu 14.12-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.12 (Ubuntu 14.12-0ubuntu0.22.04.1)
+
+-- Started on 2024-07-01 14:22:33 -03
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 CREATE TABLE public.countries (
     id_country integer NOT NULL,
     country_name character varying(100),
@@ -228,30 +243,30 @@ COPY public.countries (id_country, country_name, info, photo, country_group, tit
 --
 
 COPY public.matches (id_match, home_team_id, away_team_id, match_datetime, stadium_id, score_home_team, score_away_team, match_group) FROM stdin;
-2	2	3	2024-06-21 21:00:00	3	\N	\N	A
-3	2	4	2024-06-25 19:00:00	11	\N	\N	A
-4	3	1	2024-06-25 22:00:00	7	\N	\N	A
-5	1	2	2024-06-29 21:00:00	2	\N	\N	A
-6	4	3	2024-06-29 21:00:00	13	\N	\N	A
 1	1	4	2024-06-20 21:00:00	1	2	0	A
-8	6	7	2024-06-21 19:00:00	14	\N	\N	B
-9	5	8	2024-06-21 22:00:00	9	\N	\N	B
-10	6	8	2024-06-26 19:00:00	12	\N	\N	B
-11	7	5	2024-06-26 22:00:00	10	\N	\N	B
-12	5	6	2024-06-30 21:00:00	8	\N	\N	B
-13	8	7	2024-06-30 21:00:00	5	\N	\N	B
-14	9	12	2024-06-23 19:00:00	3	\N	\N	C
-15	10	11	2024-06-23 22:00:00	2	\N	\N	C
-16	11	9	2024-06-27 19:00:00	1	\N	\N	C
-17	10	12	2024-06-27 22:00:00	7	\N	\N	C
 18	12	11	2024-07-01 22:00:00	13	\N	\N	C
 19	9	10	2024-07-01 22:00:00	4	\N	\N	C
-20	14	15	2024-06-24 19:00:00	9	\N	\N	D
-21	13	16	2024-06-24 22:00:00	10	\N	\N	D
-22	14	16	2024-06-28 19:00:00	8	\N	\N	D
-23	15	13	2024-06-28 22:00:00	12	\N	\N	D
-24	13	14	2024-06-02 22:00:00	14	\N	\N	D
-25	16	15	2024-06-02 22:00:00	5	\N	\N	D
+24	13	14	2024-07-02 22:00:00	14	\N	\N	D
+25	16	15	2024-07-02 22:00:00	5	\N	\N	D
+2	2	3	2024-06-21 21:00:00	3	0	0	A
+3	2	4	2024-06-25 19:00:00	11	0	1	A
+4	3	1	2024-06-25 22:00:00	7	0	1	A
+5	1	2	2024-06-29 21:00:00	2	2	0	A
+6	4	3	2024-06-29 21:00:00	13	0	0	A
+8	6	7	2024-06-21 19:00:00	14	1	2	B
+9	5	8	2024-06-21 22:00:00	9	1	0	B
+10	6	8	2024-06-26 19:00:00	12	3	1	B
+11	7	5	2024-06-26 22:00:00	10	1	0	B
+12	5	6	2024-06-30 21:00:00	8	0	0	B
+13	8	7	2024-06-30 21:00:00	5	0	3	B
+14	9	12	2024-06-23 19:00:00	3	2	0	C
+15	10	11	2024-06-23 22:00:00	2	3	1	C
+16	11	9	2024-06-27 19:00:00	1	2	1	C
+17	10	12	2024-06-27 22:00:00	7	5	0	C
+20	14	15	2024-06-24 19:00:00	9	2	1	D
+21	13	16	2024-06-24 22:00:00	10	0	0	D
+22	14	16	2024-06-28 19:00:00	8	3	0	D
+23	15	13	2024-06-28 22:00:00	12	1	4	D
 \.
 
 
@@ -262,9 +277,33 @@ COPY public.matches (id_match, home_team_id, away_team_id, match_datetime, stadi
 --
 
 COPY public.players (id_player, player_name, team, photo, country, "position") FROM stdin;
-1	Lionel Messi	Argentina	url_to_messi_photo	1	Forward
-2	Sergio Agüero	Argentina	url_to_aguero_photo	1	Forward
-3	Paulo Dybala	Argentina	url_to_dybala_photo	1	Forward
+14	Marcos Acuña	Sevilla	https://media.tycsports.com/files/2022/11/01/500254/marcos-acuna-seleccion-argentina_1440x810_wmk.webp?v=1	1	Defensor
+15	Nicolas Tagliafico	OL	https://www.debrown.com.ar/wp-content/uploads/2023/09/Tagliafico-51-partidos-argentina.jpg	1	Defensor
+16	German Pezzella	Real Betis	https://media.lacapital.com.ar/p/aa9a41a25e82a90912a8d1d90e6f553d/adjuntos/205/imagenes/101/668/0101668249/german-pezzellajpeg.jpeg	1	Defensor
+17	Lucas Martinez Quarta	Fiorentina	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWE50zp0bK36b_M1HazyvI3eOEhxAyiY-9rA&s	1	Defensor
+12	Cristian Romero	Tottenham	https://a.espncdn.com/photo/2021/1201/r945033_1296x729_16-9.jpg	1	Defensor
+18	Guido Rodriguez	Real Betis	https://tmssl.akamaized.net/images/foto/galerie/guido-rodriguez-argentina-1626788242-67549.jpg?lm=1626788257	1	Mediocampista
+19	Gonzalo Montiel	Nottingham Forest	https://statics.launion.digital/2022/12/crop/6393da226c3a0__940x620.webp	1	Defensor
+20	Leandro Paredes	AS Roma	https://media.tycsports.com/files/2021/06/17/295588/leandro-paredes_1440x810_wmk.jpg?v=4	1	Mediocampista
+21	Alexis Macallister	Liverpool	https://tmssl.akamaized.net/images/foto/galerie/alexis-mac-allister-fc-liverpool-2023-1693910962-115929.jpg?lm=1693910988	1	Mediocampista
+22	Rodrigo De Paul	Atletico de Madrid	https://resizer.glanacion.com/resizer/v2/rodrigo-de-paul-cumplio-30-anos-publico-fotos-con-2RUW2FLGJZBXNBQYBRNYSCUPW4.jpg?auth=472d4105194458f16c0ec2a62633ae1da1c8423f3abd6bc5b5ca9ffdb826a645&width=880&height=586&quality=70&smart=true	1	Mediocampista
+10	Nahuel Molina	Atletico de Madrid	https://tmssl.akamaized.net/images/foto/galerie/nahuel-molina-1697214727-119313.jpg?lm=1697214743	1	Defensor
+23	Exequiel Palacios	Bayer Leverkusen	https://laseleccionargentina.com/wp-content/uploads/2024/03/Palacios-1024x576.jpg	1	Mediocampista
+24	Enzo Fernandez	Chelsea	https://laseleccionargentina.com/wp-content/uploads/2023/11/Enzo-Fernandez-1.jpg	1	Mediocampista
+25	Giovanni Lo Celso	Tottenham	https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2023%2F0329%2Fr1151437_1296x729_16%2D9.jpg	1	Mediocampista
+5	Emiliano Martinez	Aston Villa	https://tmssl.akamaized.net/images/foto/galerie/martinez-emiliano-2023-argentina-1694190167-116225.jpg?lm=1694190315	1	Arquero
+27	Lionel Messi	Inter de Miami	https://www.fifpro.org/media/fhmfhvkx/messi-world-cup.jpg?rxy=0.48356841796117644,0.31512414378031967&width=1600&height=1024&rnd=133210253587130000	1	Delantero
+8	Franco Armani	River	https://media.tycsports.com/files/2023/05/12/567348/armani_1440x810_wmk.webp	1	Arquero
+9	Geronimo Rulli	Ajax	https://media.tycsports.com/files/2022/11/02/500760/geronimo-rulli-seleccion-argentina_416x234.webp	1	Arquero
+11	Lisandro Martinez	Manchester United	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvZmxSxOXJaqIgjc5107f5x_bl5zhH-rNnaw&s	1	Defensor
+13	Nicolas Otamendi	Benfica	https://tn.com.ar/resizer/v2/nicolas-otamendi-defensor-de-la-seleccion-argentina-en-el-mundial-de-qatar-2022-OZOCUEEXBFCMJHWJNFJOVNZGAA.jpg?auth=17f08ec965ea5df0a21e4792e30e8913efcf4c64f135726ce336afade2072939&width=767	1	Defensor
+29	Alejandro Garnacho	Manchester United	https://media.tycsports.com/files/2024/03/09/688063/garnacho-manchester-united_1440x810_wmk.webp?v=1	1	Delantero
+30	Lautaro Martinez	Inter de Milan	https://laseleccionargentina.com/wp-content/uploads/2024/01/Lautaro-Martinez-Inter-vs-Milan.jpg	1	Delantero
+31	Julian Alvarez	Mancheste City	https://lmdiario.com.ar/download/multimedia.normal.af6cde1cfcc75bda.6a756c69616e20616c766172657a20466f746f204e41205245555445525320465f6e6f726d616c2e77656270.webp	1	Delantero
+26	Angel Di Maria	Benfica	https://www.elciudadanoweb.com/wp-content/uploads/2024/06/di-maria-1280x960.jpg	1	Delantero
+28	Valentin Carboni	AC Monza	https://www.topmercato.com/wp-content/uploads/2024/03/ICONSPORT_199131_0105-scaled.jpg	1	Mediocampista
+32	Nicolas Gonzalez	Fiorentina	https://cdn.eldestapeweb.com/eldestape/062024/1719232932357.webp?cw=1296&ch=729&extw=jpg	1	Delantero
+33	Vinicius Jr	Real Madrid	https://www.fifpro.org/media/fhxkcocj/imago1036849243h.jpg	13	Delantero
 \.
 
 
@@ -316,7 +355,7 @@ SELECT pg_catalog.setval('public.matches_id_match_seq', 25, true);
 -- Name: players_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.players_id_seq', 3, true);
+SELECT pg_catalog.setval('public.players_id_seq', 33, true);
 
 
 --
@@ -400,7 +439,7 @@ ALTER TABLE ONLY public.players
     ADD CONSTRAINT players_country_fkey FOREIGN KEY (country) REFERENCES public.countries(id_country);
 
 
--- Completed on 2024-06-21 16:17:59 -03
+-- Completed on 2024-07-01 14:22:33 -03
 
 --
 -- PostgreSQL database dump complete
